e636ed73acd186bab5909976ae09c77cfeb7d7c8
Клинков Георгий <semper@live.ru>
2023-12-21 11:37:45 +0300

    Работа с переменными

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("Hello, World!");
+
+double a = 3.14;
+char b = '!';
+int q = 100;
+bool d = true;
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(q);
+Console.WriteLine(d);
