476b84f58a4365ffee6cae8b00e87f23d6e0e419
Клинков Георгий <semper@live.ru>
2023-12-21 11:10:30 +0300

    Познакомились с типами переменных

--- TaskOne/Program.cs
+++ TaskOne/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Привет");
+﻿//Console.WriteLine("Привет");
+
+
+//int first = 100;
+
+//Console.WriteLine(first);
+
+double d = 19.77;
+char  q = '>';
+bool a = true;
+Console.WriteLine(d);
+Console.WriteLine(q);
+Console.WriteLine(a);
